a94d0e9c59e6555a93fe2fdd8d26b2e041855a90
yusuf-saydullayev <yusufsaydullayev@gmail.com>
2024-08-11 15:37:01 +0500

    persist

--- src/store/task.store.ts
+++ src/store/task.store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface TaskState {
      tasks: Task[];
@@ -15,53 +16,60 @@ interface Task {
      completed: boolean
 }
 
-export const useTaskStore = create<TaskState>((set, get) => ({
-     tasks: [],
-     doneTasks: [],
-     createTask: (taskTitle) => {
-          const { tasks } = get();
+export const useTaskStore = create<TaskState>()(
+     persist(
+          (set, get) => ({
+               tasks: [],
+               doneTasks: [],
+               createTask: (taskTitle) => {
+                    const { tasks } = get();
 
-          const newTask = {
-               id: crypto.randomUUID(),
-               title: taskTitle,
-               completed: false
+                    const newTask = {
+                         id: crypto.randomUUID(),
+                         title: taskTitle,
+                         completed: false
+                    }
+
+                    set({ tasks: [...tasks, newTask] })
+               },
+
+               deleteTask: (id) => {
+                    const { tasks, doneTasks } = get();
+
+                    set({
+                         tasks: tasks.filter((task) => task.id !== id),
+                         doneTasks: doneTasks.filter((task) => task.id !== id)
+                    })
+               },
+               editTask: (id, newTitle) => set((state) => ({
+                    tasks: state.tasks.map((task) => task.id === id ? { ...task, title: newTitle } : task)
+               })),
+               createTaskDone: (task) => {
+
+                    const { doneTasks, tasks } = get();
+
+                    const doneTaskExists = doneTasks.find((t) => t.id === task.id) !== undefined
+
+                    if (doneTaskExists) {
+                         set({
+                              doneTasks: doneTasks.filter((t) => t.id !== task.id),
+                              tasks: [...tasks, { ...task, completed: false }]
+                         })
+                    }
+
+                    else {
+                         set({
+                              doneTasks: [...doneTasks,
+                              { ...task, id: crypto.randomUUID(), completed: true }
+                              ],
+                              tasks: tasks.filter((t) => t.id !== task.id)
+                         })
+                    }
+
+               }
+          }),
+          {
+               name: 'tasks',
           }
-
-          set({ tasks: [...tasks, newTask] })
-     },
-
-     deleteTask: (id) => {
-          const { tasks, doneTasks } = get();
-
-          set({
-               tasks: tasks.filter((task) => task.id !== id),
-               doneTasks: doneTasks.filter((task) => task.id !== id)
-          })
-     },
-     editTask: (id, newTitle) => set((state) => ({
-          tasks: state.tasks.map((task) => task.id === id ? { ...task, title: newTitle } : task)
-     })),
-     createTaskDone: (task) => {
-
-          const { doneTasks, tasks } = get();
-
-          const doneTaskExists = doneTasks.find((t) => t.id === task.id) !== undefined
-
-          if (doneTaskExists) {
-               set({
-                    doneTasks: doneTasks.filter((t) => t.id !== task.id),
-                    tasks: [...tasks, { ...task, completed: false }]
-               })
-          }
-
-          else {
-               set({
-                    doneTasks: [...doneTasks,
-                    { ...task, id: crypto.randomUUID(), completed: true }
-                    ],
-                    tasks: tasks.filter((t) => t.id !== task.id)
-               })
-          }
-
-     }
-}))
+     )
+)
